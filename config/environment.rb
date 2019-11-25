755fec36e61a6d791f52373246e52f0c041aada9
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2019-11-25 16:08:59 -0600

    Update enviroment file for heroku production server

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,25 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :database => "note_saver_postgres_development"
-)
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "postgresql",
+    :database => "note_saver_postgres_development"
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+ 
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+ end
 
 require './app/controllers/application_controller'
 require_all 'app'
